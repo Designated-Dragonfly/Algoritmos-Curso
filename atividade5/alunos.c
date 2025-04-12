58d25210ed1a450b8e0445b90c2e4268ba02a2a0
Guilherme Santos <155643552+Designated-Dragonfly@users.noreply.github.com>
2025-04-12 11:19:53 -0300

    atividade 5 terminada

--- atividade5/alunos.c
+++ atividade5/alunos.c
@@ -21,35 +21,42 @@ int main(int argc, char const *argv[]) {
         alunos[contagem] = nota;
         
     }
+    
     int aprovados_qtd = 0;
     float media = 0.0;
-    float menor_nota = 1000.0;
-    float maior_nota = -1.0;
+    float menor_nota = 100.0;
+    float maior_nota = -1;
+
+    int maior_nota_posi;
 
     for (int i = 0; i <= qtd_alunos; i++) {
-
         media += alunos[i];
         if (alunos[i] >= 6.0) {
             aprovados_qtd++;
         }
-
-        if (alunos[i] < menor_nota) {
-            menor_nota = alunos[i];
-        }
-
-        if (alunos[i] > maior_nota) {
+        if (maior_nota < alunos[i]) {
             maior_nota = alunos[i];
         }
-        
-        
+        if (menor_nota > alunos[i]) {
+            menor_nota = alunos[i];
+        }
     }
 
     media = media / (float) (qtd_alunos + 1);
 
     printf("A quantia de alunos aprovados e: %d\n", aprovados_qtd);
-    printf("A menor nota e: %d\n", menor_nota);
-    printf("A maior nota e: %d\n", maior_nota);
+    printf("A menor nota e: %.2f\n", menor_nota);
+    printf("A maior nota e: %.2f\n", maior_nota);
     printf("A media da turma e: %.2f\n", media);
-
+    
+    printf("Escolha um aluno para ver a nota: ");
+    int aluno_escolhido;
+    scanf("%d", &aluno_escolhido);
+    while (aluno_escolhido > qtd_alunos + 1 || aluno_escolhido <= 0) {
+        printf("Aluno nao existe escolha outro: ");
+        scanf("%d", &aluno_escolhido);
+    } 
+    printf("A nota do aluno %d e %.2f", aluno_escolhido, alunos[--aluno_escolhido]);
+    
     return 0;
 }
